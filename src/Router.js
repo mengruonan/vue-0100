1f8c8c101e71a4265e77d2dfe17e8713cef60d07
mengruonan007 <m15222140404@163.com>
2019-02-23 16:06:14 +0800

    first commit

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 
-import Vue from "vue/dist/vue.js"
+import Vue from "vue/dist/vue.js" 
 import VueRouter from "vue-router"
 Vue.use(VueRouter)
 // 导入tabber部分的组件
-import HomeContainer from "./components/HomeContainer.vue"
-import MemberContainer from "./components/MemberContainer.vue"
-import ShopcarContainer from "./components/ShopcarContainer.vue"
-import SearchContainer from "./components/SearchContainer.vue"
+import HomeContainer from "./components/tabber/HomeContainer.vue"
+import MemberContainer from "./components/tabber/MemberContainer.vue"
+import ShopcarContainer from "./components/tabber/ShopcarContainer.vue"
+import SearchContainer from "./components/tabber/SearchContainer.vue"
+import NewList from  "./components/News/NewList.vue"
+
 
 let router= new VueRouter({
     routes:[
@@ -14,7 +16,8 @@ let router= new VueRouter({
         {path:"/home",component:HomeContainer},
         {path:"/member",component:MemberContainer},
         {path:"/shopcar",component:ShopcarContainer},
-        {path:"/search",component:SearchContainer}
+        {path:"/search",component:SearchContainer},
+        {path:"/home/newList",component:NewList}
     ],
     linkActiveClass:"mui-active"
 })
